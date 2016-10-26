69647a99dee6f7de2b8f3a65fbbf814f72bdffbe
ahounsel <austin.hounsel@gmail.com>
2016-10-26 12:24:14 -0500

    Produce a working parallel CUDA implementation; refactor repo

--- src/pthreads/info.h
+++
@@ -1,9 +0,0 @@
-#include <stdarg.h>
-#include <sys/time.h>
-
-static inline unsigned ticks() {
-  struct timeval tv;
-  if(gettimeofday(&tv, NULL) != 0)
-    return 0;
-  return (tv.tv_sec*1000000)+(tv.tv_usec);
-}
